f8782413531869ddf0e5996dfbd5b67c0b62030d
Mateus Sabino <mateussabino87@gmail.com>
2022-10-14 09:11:37 -0300

    feat: Configuração Inicial

--- assign.sh
+++ assign.sh
@@ -1,7 +1,5 @@
 cordova build android --release &&
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk br.gov.ba.car.cooperative && 
-# /home/mateus/Android/Sdk/build-tools/30.0.3/apksigner sign --ks-key-alias br.gov.ba.car.cooperative --ks android.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk &&
 touch app-release.apk && rm app-release.apk &&
 zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk app-release.apk &&
-/home/mateus/Android/Sdk/build-tools/30.0.3/apksigner sign --ks-key-alias br.gov.ba.car.cooperative --ks android.keystore app-release.apk &&
+/home/mateus/Android/Sdk/build-tools/30.0.3/apksigner sign --ks-key-alias br.gov.pb.cooperar.sol.associacao --ks android_associacao_pb.keystore app-release.apk &&
 /home/mateus/Android/Sdk/build-tools/30.0.3/apksigner verify -v app-release.apk
